506a12c323cd13fb9c3df194a343d992771aedc2
Chakra <chakraodba@gmail.com>
2023-06-18 13:20:45 +0530

    Added py zip file

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,7 @@
-print ("Fuck her hard")
+def lambda_handler(event, context):
+    # Lambda function logic goes here
+    print ("Fuck her hard")
+    return {
+        'statusCode': 200,
+        'body': 'Hello, from Lambda!'
+    }
