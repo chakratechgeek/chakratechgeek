6eb5c54494525c85b96e1e7cf9c4c5df0b9a1c73
chakratechgeek <89085880+chakratechgeek@users.noreply.github.com>
2023-06-18 18:53:59 +0530

    commit is pushed

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
 def lambda_handler(event, context):
     # Lambda function logic goes here
     print ("Fuck her hard")
+    print ("Chumma")
     return {
         'statusCode': 200,
         'body': 'Hello, from Lambda!'
