61767a31f563e87dfff8afd0109f2921f2d191de
chakratechgeek <89085880+chakratechgeek@users.noreply.github.com>
2023-06-18 19:52:48 +0530

    1st commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 def lambda_handler(event, context):
     # Lambda function logic goes here
     print ("Fuck her hard")
-    print ("Chumma chakra")
+    print ("Chumma chakra ggh")
     return {
         'statusCode': 200,
         'body': 'Hello, from Lambda!'
