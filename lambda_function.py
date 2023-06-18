23269e780ab53f69de8c6e08fc1e1cc980eb929b
chakratechgeek <89085880+chakratechgeek@users.noreply.github.com>
2023-06-18 19:58:30 +0530

    qa ommit

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 def lambda_handler(event, context):
     # Lambda function logic goes here
     print ("Fuck her hard")
-    print ("Chumma chakra its dev")
+    print ("Chumma chakra its qa")
     return {
         'statusCode': 200,
         'body': 'Hello, from Lambda!'
